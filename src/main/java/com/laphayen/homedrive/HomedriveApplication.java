34c1045ea445a87afdb12222c3d5d9af50b7bf3f
Baek Gi Chan <laphayen@gmail.com>
2025-07-29 23:54:24 +0900

    #4 - refactor: remove DataSource and JPA auto-configuration exclusions
    Enabled default auto-configuration for DataSource and Hibernate JPA

--- src/main/java/com/laphayen/homedrive/HomedriveApplication.java
+++ src/main/java/com/laphayen/homedrive/HomedriveApplication.java
@@ -5,12 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication(
-		exclude = {
-				DataSourceAutoConfiguration.class,
-				HibernateJpaAutoConfiguration.class
-		}
-)
+@SpringBootApplication
 public class HomedriveApplication {
 
 	public static void main(String[] args) {
